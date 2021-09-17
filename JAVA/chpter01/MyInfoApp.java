79af5d54f85da4aa058cd22f81adfc223b0fad86
nar0ng <criticalpxint@naver.com>
2021-09-17 12:12:10 +0900

    JAVA chpter01

---
+++ JAVA/chpter01/MyInfoApp.java
@@ -0,0 +1,12 @@
+
+public class MyInfoApp {
+
+	public static void main(String[] args) {
+	System.out.println("Kim NaRyung");
+	System.out.println(22+" years old");
+	System.out.println("Departmanet of Computer Engineering");
+	
+
+	}
+
+}
